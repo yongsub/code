e4d007fd61b7556c231cd3991d80f4f3502de3b3
yongsub <ylim.mail@gmail.com>
2017-06-18 13:07:10 +0900

    data_collection for Windows

--- preco/korbit/data/data_collection.py
+++ preco/korbit/data/data_collection.py
@@ -1,32 +1,30 @@
-import sys, os, json, time, wget
+import sys, os, json, time, urllib, wget, datetime
 
-# CMD = 'wget "https://api.korbit.co.kr/v1/transactions?currency_pair={}&time=minute" -q -O data/tmp.json'
-URL = 'https://api.korbit.co.kr/v1/transactions?currency_pair={}&time=minute'
-TIME_INTERVAL = 1
+currency = sys.argv[1] # target \in [eth_krw, btc_krw]
 
-# argv = sys.argv
-argv = [None, 'eth_krw']
+TMP_JSON = '.tmp/{}.json'.format(currency)
+CMD = 'wget "https://api.korbit.co.kr/v1/transactions?currency_pair={}&time=minute" -q -O {}'
+TIME_INTERVAL = 20
 
-currency = argv[1]  # eth_krw or btc_krw
-log_file = 'data/ethereum/' + currency + '_log'
+log_file_prefix = '{}/{}'.format(currency, currency)
 log_fields = [u'timestamp', u'tid', u'price', u'amount']
-
-err_log_file = log_file + '_err'
 
 n_last_few = 1000
 last_few_tids = []
 
 while True:
     try:
-        # this_cmd = CMD.format(currency)
-        # os.system(this_cmd)
-        wget.download(url=URL, out='tmp.json')
+        now = datetime.datetime.now().strftime('%y-%m-%d')
 
-        with open('data/tmp.json', 'r') as f_tmp:
+        this_cmd = CMD.format(currency, TMP_JSON)
+        os.system(this_cmd)
+
+        with open(TMP_JSON, 'r') as f_tmp:
             transactions_json = f_tmp.read()
 
         transactions = json.loads(transactions_json)
 
+        log_file = '{}_{}'.format(log_file_prefix, now) 
         with open(log_file, 'a') as f:            
             new_transactions = filter(lambda tr: tr['tid'] not in last_few_tids, transactions)
             new_records = map(lambda new_tr: ','.join([str(new_tr[key]) for key in log_fields]), new_transactions)
@@ -34,12 +32,6 @@ while True:
             if len(new_records) > 0:
                 new_records_in_string = '\n'.join(new_records[::-1])
                 f.write(new_records_in_string + '\n')
-                
-                new_prices = [new_tr['price'] for new_tr in new_transactions]
-                min_max = [int(min(new_prices)), int(max(new_prices))]
-                min_max_diff = min_max + [min_max[1] - min_max[0]]
-            else:
-                min_max_diff = [None] * 3
     
             last_few_tids += [new_tr['tid'] for new_tr in new_transactions]
             last_few_tids = last_few_tids[-n_last_few:]
